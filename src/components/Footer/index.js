4279ff118822edd6ae266ec69121d806c1cc5eb6
AniaCuapio <ania.cuapio@gmail.com>
2020-10-13 17:24:10 -0500

    footer ready

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,16 +6,18 @@ export default function Footer(){
     return (
         <div className={styles.container}>
                 <div className={styles.firstContainer}>
-                <p>2014 Ingenia Group</p>
+                <br/>
+                <p> © 2014 Ingenia Group</p>
                 <h6> USA </h6>
                 <p>1200 18th Street, NW </p>
                 <p>Suite 700</p>
                 <p> Washington DC 20036 USA</p>
                 <p>50220900</p>
                 </div>
-                <Divider type="vertical" />
+                <hr/>
                 <div className={styles.secondContainer}>
-                <p></p>
+                <br/>
+                <br/>
                 <h6> MÉXICO </h6>
                 <p>Anatole France 311</p>
                 <p>Polanco, Miguel Hidalgo</p>
@@ -23,24 +25,30 @@ export default function Footer(){
                 <p>50220900</p>
                 </div>
                 <div className={styles.thirdContainer}>
+                <br/>
+                <br/>
                 <div className={styles.innerContainer}>
                 <p> About us</p>
+                <hr/>
                 <p> Our Work</p>
+                <hr/>
                 <p> Blog</p>
+                <hr/>
                 <p> Jobs</p>
+                <hr/>
                 <p> Contact</p>
                 </div>
                 <div className={styles.secondInnerContainer}>
                     <p>Terms of use</p>
+                    <hr/>
                     <p> Privacy Policy</p>
                 </div>
-            
+        
                 <div className={styles.iconsContainer}>
                     <div className={styles.icon}><img src="/twitter-brands.svg" /></div>
                     <div className={styles.icon}><img src="/facebook-f-brands.svg" /></div>
                     <div className={styles.icon}><img src="/caret-right-solid.svg" /></div>
                     <div className={styles.icon}><img src="/linkedin-in-brands.svg" /></div>
-
                 </div>
 
 
